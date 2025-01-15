042dcc94fcc0cf21789f1d5b540f1fa5c9d1de60
mikel161211231 <mikel.lopez@ikasle.aeg.eus>
2025-01-15 11:05:23 +0100

    [UPDATE] The test for filterByLevelRequirement

--- src/__tests__/potionHelpers.test.ts
+++ src/__tests__/potionHelpers.test.ts
@@ -4,12 +4,19 @@ import { filterByLevelRequirement } from "../helpers/potionHelpers"
 
 
 describe('Test the function filterByLevelRequirement', () => {
-    it('Test if the potions with level requirement are under 37', () => {
-        const filteredPotions = filterByLevelRequirement(potions, 37);
-        filteredPotions.map( potion => expect(potion.usage.restrictions.levelRequirement).toBeLessThanOrEqual(37))
+    it('Test if the potions with level requirement are under 35 and are 10', () => {
+        const filteredPotions = filterByLevelRequirement(potions, 35);
+        filteredPotions.map( potion => expect(potion.usage.restrictions.levelRequirement).toBeLessThanOrEqual(35));
+        expect(filteredPotions.length).toBe(10);
     })
-    it('Test if the potions with level requirement are under 0', () => {
+    it('Test if the potions with level requirement are under 17 and are 3', () => {
+        const filteredPotions = filterByLevelRequirement(potions, 17);
+        filteredPotions.map( potion => expect(potion.usage.restrictions.levelRequirement).toBeLessThanOrEqual(17));
+        expect(filteredPotions.length).toBe(3);
+    })
+    it('Test if the potions with level requirement are under 0 and are 0', () => {
         const filteredPotions = filterByLevelRequirement(potions, 0);
-        filteredPotions.map( potion => expect(potion.usage.restrictions.levelRequirement).toBeLessThanOrEqual(0))
+        filteredPotions.map( potion => expect(potion.usage.restrictions.levelRequirement).toBeLessThanOrEqual(0));
+        expect(filteredPotions.length).toBe(0);
     })
 })
