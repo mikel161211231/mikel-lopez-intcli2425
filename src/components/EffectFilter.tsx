531004cfb4f3c7f2b0e9f0ad7f1e9b3bb9681949
mikel161211231 <mikel.lopez@ikasle.aeg.eus>
2025-01-17 07:48:30 +0100

    [NEW] Create the effect filter

---
+++ src/components/EffectFilter.tsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import { filterByLevelRequirement, findPotionByEffect, getPotionsByRarity } from "../helpers/potionHelpers";
+import { Potion } from "../types/Potion";
+
+
+
+interface EffectFilterProps {
+    // potions: Potion[];
+    allPotions: Potion[];
+    setPotionList: Function;
+    rarityFilter: string;
+    levelFilter: number;
+    // effectFilter: string
+    setEffectFilter: Function;
+}
+
+const EffectFilter: React.FC<EffectFilterProps> = ({ setPotionList, allPotions, rarityFilter, setEffectFilter, levelFilter }) => {
+
+    const [secondaryEffect, setSecondaryEffect] = useState<string>("");
+
+    const filterByEffect = (effect:string) => {
+
+        let filteredPotions: Potion[] = [];
+            if (rarityFilter !== "") {
+                filteredPotions = getPotionsByRarity(allPotions, rarityFilter);
+                filteredPotions = filterByLevelRequirement(filteredPotions, levelFilter);
+                filteredPotions = findPotionByEffect(filteredPotions, effect);
+            } else {    
+                filteredPotions = filterByLevelRequirement(allPotions, levelFilter);  
+                filteredPotions = findPotionByEffect(filteredPotions, effect);
+            }  
+        setSecondaryEffect(effect);
+        setEffectFilter(effect);
+        setPotionList(filteredPotions);
+    }
+
+    return (
+        <>
+            <div className="m-4">
+                <label htmlFor="effect" className="text-darkSepia text-xl  ">Effect filter </label>
+                <input type="text" id="effect" value={secondaryEffect} onChange={(effect) => {filterByEffect(effect.target.value)}} />
+            </div>
+
+        </>
+    );
+};
+
+export default EffectFilter;
