fe28de6c6ecd16674acc23d06e1912bbe3d39b91
mikel161211231 <mikel.lopez@ikasle.aeg.eus>
2025-01-17 07:43:24 +0100

    [UPDATE] Update the rarity filter

--- src/components/RarityFilter.tsx
+++ src/components/RarityFilter.tsx
@@ -1,22 +1,75 @@
 import { useState } from "react";
-import { getPotionsByRarity } from "../helpers/potionHelpers";
+import { filterByLevelRequirement, findPotionByEffect, getPotionsByRarity } from "../helpers/potionHelpers";
 import { Potion } from "../types/Potion";
 
 
 
 interface RarityFilterProps {
     potions: Potion[];
+    allPotions: Potion[];
     setPotionList: Function;
+    rarityFilter: string;
+    setRarityFilter: Function;
+    levelFilter: number;
+    effectFilter: string;
 }
 
-const RarityFilter: React.FC<RarityFilterProps> = ({ potions, setPotionList }) => {
+const RarityFilter: React.FC<RarityFilterProps> = ({ potions, setPotionList, allPotions, rarityFilter, setRarityFilter, levelFilter, effectFilter }) => {
 
     const [rarity, setRarity] = useState<string>("");
     const [openOptions, setOpenOptions] = useState<boolean>(false);
 
-    const filterThePotionsWithRarity = () => {
-        const filteredPotions = getPotionsByRarity(potions, rarity);
+    const filterThePotionsWithLegendaryRarity = () => {
+
+        let filteredPotions: Potion[] = [];
+
+        if (rarityFilter === "") {
+            filteredPotions = getPotionsByRarity(potions, "legendary");
+
+        } else {
+            filteredPotions = filterByLevelRequirement(allPotions, levelFilter);
+             if (effectFilter !== "") {
+                filteredPotions = findPotionByEffect(filteredPotions, effectFilter);
+            }
+            filteredPotions = getPotionsByRarity(filteredPotions, "legendary");
+        }
+        setRarity("Legendary");
+        setRarityFilter("legendary");
         setPotionList(filteredPotions);
+
+    }
+
+    const filterThePotionsWithEpicRarity = () => {
+        let filteredPotions: Potion[] = [];
+        if (rarityFilter === "") {
+            filteredPotions = getPotionsByRarity(potions, "epic");
+        } else {
+            filteredPotions = filterByLevelRequirement(allPotions, levelFilter);
+             if (effectFilter !== "") {
+                filteredPotions = findPotionByEffect(filteredPotions, effectFilter);
+            }
+            filteredPotions = getPotionsByRarity(filteredPotions, "epic");
+        }
+        setRarity("Epic")
+        setRarityFilter("epic");
+        setPotionList(filteredPotions);
+    }
+
+    const filterThePotionsWithMythicRarity = () => {
+        let filteredPotions: Potion[] = [];
+        if (rarityFilter === "") {
+            filteredPotions = getPotionsByRarity(potions, "mythic");
+        } else {
+            filteredPotions = filterByLevelRequirement(allPotions, levelFilter);
+             if (effectFilter !== "") {
+                filteredPotions = findPotionByEffect(filteredPotions, effectFilter);
+            }
+            filteredPotions = getPotionsByRarity(filteredPotions, "mythic");
+            
+        }
+        setRarity("Mythic")
+            setRarityFilter("mythic");
+            setPotionList(filteredPotions);
     }
 
     const openDropdownOptions = () => {
@@ -24,18 +77,32 @@ const RarityFilter: React.FC<RarityFilterProps> = ({ potions, setPotionList }) =
     }
 
 
+
     return (
         <>
-    <div>
-        {openOptions && (
-            <div>
-            <button onClick={openDropdownOptions}>Legendary</button>
-            <button onClick={openDropdownOptions}>Epic</button>
-            <button onClick={openDropdownOptions}>Mythic</button>
+            <div className="m-4">
+                {(rarity === "") ? (
+                    <button onClick={openDropdownOptions} className="w-64 text-xl text-darkSepia border-2  border-black pl-6 pr-6 hover:bg-medievalRed">Rarity</button>
+                )
+                    :
+                    (
+                        <button onClick={openDropdownOptions} className=" w-64 text-xl text-darkSepia border-2  border-black pl-6 pr-6 hover:bg-medievalRed">Selected {rarity}</button>
+                    )}
+
+                {openOptions && (
+                    <div className="z-20">
+                        <div>
+                            <button onClick={filterThePotionsWithLegendaryRarity} className="w-64 text-medievalSepia border-2  border-black pl-6 pr-6 hover:bg-darkSepia">Legendary</button>
+                        </div>
+                        <div>
+                            <button onClick={filterThePotionsWithEpicRarity} className="w-64 text-medievalSepia border-2  border-black pl-6 pr-6 hover:bg-darkSepia">Epic</button>
+                        </div>
+                        <div>
+                            <button onClick={filterThePotionsWithMythicRarity} className="w-64 text-medievalSepia border-2 border-black pl-6 pr-6 hover:bg-darkSepia">Mythic</button>
+                        </div>
+                    </div>
+                )}
             </div>
-        )}
-  <button onClick={openDropdownOptions}>R</button>
-</div>
 
         </>
     );
